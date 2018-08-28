ad572f864719743235fc3caef56a671e517be9a2
Yudha Pratama <yudha.webdev@gmail.com>
2018-08-28 12:46:11 +0700

    Fixed migrateMysql and added setting engine, charset and collation to config json

--- index.js
+++ index.js
@@ -23,26 +23,33 @@ function checkDirExists(callback) {
 function makeConfigDB(driver, dbName) {
   if (driver !== 'mysql' && driver !== 'mongodb') {
     process.stdout.write('Sorry, moongarmjs-cli only support MySql or MongoDB\n');
+    process.exit();
   } else {
     const migrationText = {
+      connection: driver,
       database: dbName,
-      engine: driver,
       host: '127.0.0.1',
       port: '3306',
       username: 'root',
       password: '',
+      engine: 'InnoDB',
+      charset: 'utf8',
+      collation: 'utf8_unicode_ci',
       migrationDir: `${process.cwd()}/database/migrations`,
       seederDir: `${process.cwd()}/database/seeders`,
     };
 
     if (fs.existsSync(`${process.cwd()}/moongarm-dbconfig.json`)) {
       process.stdout.write('Sorry, database config already exists.\n');
+      process.exit();
     } else {
       fs.writeFile(`${process.cwd()}/moongarm-dbconfig.json`, JSON.stringify(migrationText, null, 2), (err) => {
         if (err) {
           process.stdout.write('Sorry :(, database config has failed to be created.\n');
+          process.exit();
         } else {
           process.stdout.write('Database config was successfully created.\n');
+          process.exit();
         }
       });
     }
@@ -66,14 +73,18 @@ function makeMigrationFile(name) {
   checkDirExists(() => {
     if (fs.existsSync(`${process.cwd()}/database/migrations/${name}.json`)) {
       process.stdout.write('Sorry, migration file already exists.\n');
+      process.exit();
     } else if (!fs.existsSync(`${process.cwd()}/moongarm-dbconfig.json`)) {
       process.stdout.write('Sorry, database config not exists.\nPlease create dbconfig with run command\n\'moongarm-cli make:config <db_type> <database_name>\'\n');
+      process.exit();
     } else {
       fs.writeFile(`${process.cwd()}/database/migrations/${name}.json`, JSON.stringify(migrationText, null, 2), (err) => {
         if (err) {
           process.stdout.write('Sorry :(, migration file has failed to be created.\n');
+          process.exit();
         } else {
           process.stdout.write(`Migration ${name} was successfully created.\n`);
+          process.exit();
         }
       });
     }
@@ -81,15 +92,32 @@ function makeMigrationFile(name) {
 }
 
 function migrateMysql() {
-  // const files = fs.readdirSync(`${process.cwd()}/database/migrations`);
-  // files.forEach((file) => {
-  //   if (!fs.statSync(`${process.cwd()}/database/migrations/${file}`).isDirectory()) {
-  //     fs.readFile(`${process.cwd()}/database/migrations/${file}`, 'utf8', (err, data) => {
-  //       //
-  //     });
-  //   }
-  // });
-  mysql.connection();
+  mysql.connection()
+    .then((data) => {
+      if (data === null) {
+        checkDirExists(() => {
+          if (!fs.existsSync(`${process.cwd()}/moongarm-dbconfig.json`)) {
+            process.stdout.write('Sorry, database config not exists.\nPlease create dbconfig with run command\n\'moongarm-cli make:config <db_type> <database_name>\'\n');
+            process.exit();
+          } else {
+            fs.readdirSync(`${process.cwd()}/database/migrations/`).forEach((file) => {
+              mysql.createTable(file)
+                .then(() => {
+                  process.stdout.write('Migration finished.\n');
+                  process.exit();
+                })
+                .catch((err) => {
+                  process.stdout.write(`Migration failed with error: ${err.message}`);
+                  process.exit();
+                });
+            });
+          }
+        });
+      }
+    })
+    .catch(() => {
+      process.exit();
+    });
 }
 
 program
