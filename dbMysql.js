6a6fca455543ffab28a03fe97b1bd21ea193edf5
Yudha Pratama <yudha.webdev@gmail.com>
2018-08-28 09:31:03 +0700

    Fixed mysql connection

--- dbMysql.js
+++ dbMysql.js
@@ -1,22 +1,32 @@
+const fs = require('fs');
 const mysql = require('mysql');
 
 function Mysql() { }
 
 Mysql.connection = function connection() {
   return new Promise((resolve, reject) => {
-    mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'ccpos',
-    }).connect((err) => {
-      if (err) {
-        const errMessage = `Database connection error: ${err.stack}`;
-        reject(errMessage);
-      } else {
-        resolve();
-      }
-    });
+    if (fs.existsSync(`${process.cwd()}/moongarm-dbconfig.json`)) {
+      fs.readFile(`${process.cwd()}/moongarm-dbconfig.json`, 'utf8', (err, data) => {
+        if (err) {
+          process.stdout.write(`Sorry, dbconfig cannot read: ${err}`);
+          reject();
+        } else {
+          const configJson = JSON.parse(data);
+          mysql.createConnection({
+            host: configJson.host,
+            port: configJson.port,
+            user: configJson.username,
+            password: configJson.password,
+            database: configJson.database,
+          }).connect((status) => {
+            resolve(status);
+          });
+        }
+      });
+    } else {
+      process.stdout.write('Sorry, database config not exists.\nPlease create dbconfig with run command\n\'moongarm-cli make:config <db_type> <database_name>\'\n');
+      reject();
+    }
   });
 };
 
